8011fbb70d765713d54dd12fb020b1da9e937eb9
riku213 <kotatorameme3@gmail.com>
2025-10-14 14:59:34 +0900

    画面を４分割

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def menu_link_to(item)
+        if current_page?(item[:link])
+            raw "<span class='current_page'>" + item[:name] + "</span>"
+        elsif item[:disabled]
+            raw "<span class='disabled'>" + item[:name] + '(' + (t 'not_ready') + 
+            ')</span>'
+        else
+            link_to( item[:name], item[:link] )
+        end
+    end
 end
